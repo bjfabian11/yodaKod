6089f151feb711ee0518ca4aecd99e8b23b1dc08
bjfabian11 <43792954+bjfabian11@users.noreply.github.com>
2019-05-28 01:45:15 -0400

    accidentally uploaded main folder
    accidentally uploaded main folder and not content folders. Added auth, login, welcome page, and secured navbar links to view only when logged in or logged out

--- controllers/createPost.js
+++ controllers/createPost.js
@@ -1,3 +1,7 @@
-module.exports = (req, res)=>{
-	res.render('create');
+module.exports = (req, res) => {
+    if (req.session.userId) {
+        return res.render("create");
+    }
+ 
+    res.redirect('/auth/login')
 };
