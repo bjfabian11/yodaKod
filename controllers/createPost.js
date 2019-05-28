f46947755d446a1152478e56e2b00eb068d4002b
bjfabian11 <43792954+bjfabian11@users.noreply.github.com>
2019-05-28 01:49:47 -0400

    Revert "accidentally uploaded main folder"
    This reverts commit 6089f151feb711ee0518ca4aecd99e8b23b1dc08.

--- controllers/createPost.js
+++ controllers/createPost.js
@@ -1,7 +1,3 @@
-module.exports = (req, res) => {
-    if (req.session.userId) {
-        return res.render("create");
-    }
- 
-    res.redirect('/auth/login')
+module.exports = (req, res)=>{
+	res.render('create');
 };
--- middleware/redirectIfAuthenticated.js
+++
@@ -1,9 +0,0 @@
-const User = require('../database/models/User')
- 
-module.exports = (req, res, next) => {
-    if (req.session.userId) {
-        return res.redirect('/')
-    }
- 
-    next()
-}
